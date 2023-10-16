b65195c476fa56fedd30cf209cec5b0d3f791b9a
tranggithub <20520323@gm.uit.edu.vn>
2023-10-16 11:40:26 +0700

    BottomNavigation

---
+++ app/src/main/java/com/example/travel_tour_booking_app/SearchFragment.java
@@ -0,0 +1,21 @@
+package com.example.travel_tour_booking_app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SearchFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
