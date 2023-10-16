c90cece7fab63387770dc6f7d0d6c8ba23470f7d
tranggithub <20520323@gm.uit.edu.vn>
2023-10-17 04:44:24 +0700

    change color selected item in navigation bar

--- app/src/main/java/com/example/travel_tour_booking_app/HomeActivity.java
+++ app/src/main/java/com/example/travel_tour_booking_app/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.ClipData;
 import android.os.Bundle;
 
 import com.example.travel_tour_booking_app.databinding.ActivityHomeBinding;
