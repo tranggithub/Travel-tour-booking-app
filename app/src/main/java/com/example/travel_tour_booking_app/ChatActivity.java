1468fccbecedefce1d0c329ccccfc6b4be94c802
LTThuyLinh <letranthuylinh1003@gmail.com>
2023-10-31 13:44:52 +0700

    update ChatActivity

--- app/src/main/java/com/example/travel_tour_booking_app/ChatActivity.java
+++ app/src/main/java/com/example/travel_tour_booking_app/ChatActivity.java
@@ -1,14 +1,49 @@
 package com.example.travel_tour_booking_app;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class ChatActivity extends AppCompatActivity {
-
+    EditText edtUserType;
+    ImageView ivSendButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        ChatFragment chatFragment = new ChatFragment();
+        replaceFragment(chatFragment);
+
+        edtUserType = findViewById(R.id.edt_message_chat);
+        ivSendButton = findViewById(R.id.iv_sendbutton_chat);
+        ivSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the user's message from the EditText
+                if (edtUserType.getText().toString()!=null)
+                {
+                    String userTypeMsg = edtUserType.getText().toString();
+
+                    // Call the function to add the user's message to the chat
+                    chatFragment.addUserMessageToChat(userTypeMsg);
+
+                    // Clear the input field after sending
+                    edtUserType.setText("");
+                }
+            }
+        });
+    }
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fl_chat, fragment);
+        fragmentTransaction.commit();
     }
 }
