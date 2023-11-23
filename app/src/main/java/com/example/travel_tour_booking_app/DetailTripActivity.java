37dfb4b594c7a50793087d610ef66b58720ca419
tranggithub <20520323@gm.uit.edu.vn>
2023-11-23 11:02:16 +0700

    fix frontend trip detail

--- app/src/main/java/com/example/travel_tour_booking_app/DetailTripActivity.java
+++ app/src/main/java/com/example/travel_tour_booking_app/DetailTripActivity.java
@@ -8,35 +8,39 @@ import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class DetailTripActivity extends Activity {
+public class DetailTripActivity extends AppCompatActivity {
     ArrayList<DetailNews> detailTripArrayList;
     DetailNewsAdapter detailTripAdapter;
-
-    public void ScrollToTop()
-    {
-        Button button = findViewById(R.id.btn_detail_trip_up);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ScrollView scrollView = findViewById(R.id.sv_detail_trip);
-                scrollView.fullScroll(ScrollView.FOCUS_UP);
-            }
-        });
-
-    }
-    public void GoBack()
-    {
-        ImageView imageView = findViewById(R.id.iv_returnbutton_list_trip);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_trip_detail);}
+//    public void ScrollToTop()
+//    {
+//        Button button = findViewById(R.id.btn_detail_trip_up);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ScrollView scrollView = findViewById(R.id.sv_detail_trip);
+//                scrollView.fullScroll(ScrollView.FOCUS_UP);
+//            }
+//        });
+//
+//    }
+//    public void GoBack()
+//    {
+//        ImageView imageView = findViewById(R.id.iv_returnbutton_list_trip);
+//        imageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                finish();
+//            }
+//        });
+//    }
 }
--- app/src/main/java/com/example/travel_tour_booking_app/TourDetailActivity.java
+++ app/src/main/java/com/example/travel_tour_booking_app/TourDetailActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.travel_tour_booking_app.databinding.ActivityMainBinding;
+public class TourDetailActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour_detail);
-
-
     }
 }
